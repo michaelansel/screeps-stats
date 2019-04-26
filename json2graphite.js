a1faaa8fdee1a9b4d83a26fb4e3097c51f5d1470
Michael Ansel <mransel@twitch.tv>
2019-04-25 17:20:45 -0700

    Convert to streams
    Without streams, data is processed as quickly as possible and queued up as
    async events waiting for processing (which are processed at the rate of the
    receiving pipe). If the receiving pipe is to SSD, this is fine. If it is to
    gzip, then we run out of heap in node.js very quickly. Streams apply
    backpressure to the input and slow down reading to only process at the rate
    the output pipe is able to accept.

--- json2graphite.js
+++ json2graphite.js
@@ -1,28 +1,96 @@
-const readline = require('readline');
-
-function flattenMetrics(object, prefix="", timestamp="") {
-  return Object.keys(object).sort().map(function (k){
+function flattenMetrics(object, prefix="", timestamp="", cb) {
+  Object.keys(object).sort().forEach(function (k){
     let metric = k;
     if (prefix.length > 0) metric = prefix + "." + metric;
     if (typeof object[k] === 'object') {
-      return flattenMetrics(object[k], metric, timestamp);
+      flattenMetrics(object[k], metric, timestamp, cb);
     } else {
-      return [metric, object[k], timestamp].join(' ');
+      cb( [metric, object[k], timestamp].join(' ') + "\n" );
     }
-  }).join('\n');
+  });
 }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
+const { pipeline, Transform } = require('stream');
+const util = require('util');
 
-rl.on('line', function (line) {
- if (line.length > 2) {
-   try {
-     const stats = JSON.parse(line);
-     console.log(flattenMetrics(stats, "screeps.USERNAME", Math.floor(stats.runtime.time/1000)));
-   } catch (err) { console.error(err, line); }
- }
-});
+function ReadlineStream(options) {
+  Transform.call(this, options);
+
+  // use objectMode to stop the output from being buffered
+  // which re-concatanates the lines, just without newlines.
+  if (!options) options = {}; // ensure object
+  options.objectMode = true; // forcing object mode
+
+  this.lineBuffer = '';
+
+  // take the source's encoding if we don't have one
+  this.on('pipe', function(src) {
+    if (!this.encoding) {
+      this.encoding = src._readableState.encoding;
+    }
+  });
+}
+util.inherits(ReadlineStream, Transform);
+
+ReadlineStream.prototype._transform = function(chunk, encoding, done) {
+  // decode binary chunks as UTF-8
+  if(Buffer.isBuffer(chunk))
+  {
+    if(!encoding || encoding == 'buffer') encoding = 'utf8';
+
+    chunk = chunk.toString(encoding);
+  }
+
+  this.lineBuffer += chunk;
+  var lines = this.lineBuffer.split(/[\r\n]+/);
+
+  while(lines.length > 1)
+    this.push(lines.shift())
+
+  this.lineBuffer = lines[0] || '';
+
+  done();
+};
+
+ReadlineStream.prototype._flush = function(done) {
+  if(this.lineBuffer)
+  {
+    this.push(this.lineBuffer)
+    this.lineBuffer = ''
+  }
+
+  done();
+};
+
+function FlattenMetrics(options) {
+  Transform.call(this, options);
+  if (!options) options = {}; // ensure object
+  options.objectMode = true; // forcing object mode
+}
+util.inherits(FlattenMetrics, Transform);
+
+FlattenMetrics.prototype._transform = function (line, enc, cb) {
+  if (line.length > 2) {
+    try {
+      const stats = JSON.parse(line);
+      flattenMetrics(stats, "screeps.michaelansel", Math.floor(stats.runtime.time/1000), this.push.bind(this));
+    } catch (err) { console.error(err, line); }
+  }
+  cb();
+};
+
+pipeline(
+  process.stdin,
+  // Break by line
+  new ReadlineStream(),
+  // Translate JSON line into metric lines
+  new FlattenMetrics(),
+  process.stdout,
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed', err);
+    } else {
+      console.log('Pipeline succeeded');
+    }
+  }
+);
